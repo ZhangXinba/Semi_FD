7545f80521754b38a07ffcaca7d9b06855bde61c
ZhangXinba <106074055+ZhangXinba@users.noreply.github.com>
2022-05-24 11:15:57 +0800

    Add files via upload

---
+++ CIFAR/data-local/labels/bin/prepare_semisupervised_sets.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Prepare semisupervised datasets needed in the experiments
+
+SCRIPT=./create_balanced_semisupervised_labels.sh
+
+create ()
+{
+    for LABELS_PER_CLASS in ${LABEL_VARIATIONS[@]}
+    do
+        LABELS_IN_TOTAL=$(( $LABELS_PER_CLASS * $NUM_CLASSES ))
+        echo "Creating sets for $DATANAME with $LABELS_IN_TOTAL labels."
+        for IDX in {00..19}
+        do
+            LABEL_DIR=labels/${DATANAME}/${LABELS_IN_TOTAL}_balanced_labels
+            mkdir -p $LABEL_DIR
+            $SCRIPT $DATADIR $LABELS_PER_CLASS > $LABEL_DIR/${IDX}.txt
+        done
+    done
+}
+
+DATADIR=data/images/cifar/cifar10/by-image/
+DATANAME=cifar10
+NUM_CLASSES=10
+LABEL_VARIATIONS=(100 400)
+create
+
+#DATADIR=/root/Desktop/Semi-KD/CIFAR-100/data-local/images/cifar/cifar100/by-image/
+#DATANAME=cifar100
+#NUM_CLASSES=100
+#LABEL_VARIATIONS=(100 400)
+#create
+
+#DATADIR=data/images/ilsvrc2012/
+#DATANAME=ilsvrc2012
+#NUM_CLASSES=1000
+#LABEL_VARIATIONS=(128)
+#create
